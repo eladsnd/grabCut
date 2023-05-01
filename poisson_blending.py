fcaed19ea8af2568d65f6d1b67d269fba882a2a9
Yotam Hazan <yotkes1@gmail.com>
2023-05-01 13:40:25 +0300

    poisson - 2 python files

--- poisson_blending.py
+++ poisson_blending.py
@@ -1,12 +1,11 @@
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
-import scipy.sparse
-from scipy.sparse.linalg import spsolve
 import argparse
-from scipy.sparse import csr_matrix, diags, kron
+from scipy import sparse, ndimage
+from scipy.sparse import diags
 from scipy.sparse.linalg import spsolve
-
+from scipy.sparse import spdiags, eye, lil_matrix
+from scipy.sparse.linalg import spsolve
 
 # def poisson_blend(im_src, im_tgt, im_mask, center):
 #     # TODO: Implement Poisson blending of the source image onto the target ROI
@@ -15,17 +14,112 @@ from scipy.sparse.linalg import spsolve
 #     return im_blend
 
 
-def laplacian_operator(n):
+def compute_laplacian(img):
+    # Define Laplacian kernel
+    kernel = np.array([[0, 1, 0], [1, -4, 1], [0, 1, 0]])
+
+    # Compute Laplacian using convolution with kernel
+    laplacian = ndimage.convolve(img.astype(np.float32), kernel)
+
+    return laplacian
+
+def create_laplacian_matrix(width, height):
+    n = width * height
+    d = np.ones(n)
+    d[width-1::width] = 0
+    d[-width:] = 0
+    d[0:width] = 0
+    d[0::width] = 0
+
+    diags = np.array([0, 1, -1, width, -width])
+    laplacian = spdiags([-4*d, d, d, d, d], diags, n, n)
+    return laplacian
+
+def create_boundary_vector(channel_src, channel_tgt, mask, center, laplacian):
     """
-    Computes the 2D Laplacian operator for an n x n image.
+    Creates the boundary vector for a single color channel.
+
+    Args:
+        channel_src: numpy array representing the source image channel.
+        channel_tgt: numpy array representing the target image channel.
+        mask: numpy array representing the binary mask of the source region.
+        center: tuple representing the center of the source region.
+        laplacian: sparse matrix representing the Laplacian operator.
+
+    Returns:
+        numpy array representing the boundary vector.
     """
-    lap = diags([-4, 1, 1], [0, -1, 1], shape=(n, n)).tolil()
-    lap[0, 1] = 2
-    lap[-1, -2] = 2
-    return csr_matrix(kron(lap, diags([1], [0], shape=(n, n))))
+    h, w = channel_src.shape[:2]
+    b = np.zeros((h, w))
+
+    # find the indices of the source region in the mask
+    indices = np.nonzero(mask)
+
+    # calculate the Laplacian of the target region
+    laplacian_tgt = laplacian.dot(channel_tgt.flatten())
+
+    # calculate the boundary vector
+    for x, y in zip(indices[0], indices[1]):
+        if y == 0 or y == w-1 or x == 0 or x == h-1:
+            # pixel is on the border
+            b[x, y] = channel_src[x, y]
+        else:
+            # pixel is not on the border, calculate the Laplacian of the source region
+            laplacian_src = 4 * channel_src[x, y] - channel_src[x-1, y] - channel_src[x+1, y] \
+                            - channel_src[x, y-1] - channel_src[x, y+1]
+
+            # calculate the boundary value
+            b[x, y] = laplacian_tgt[x*w + y] - laplacian_src
+
+    # set the center pixel value
+    # center_x, center_y = center
+    # b[center_y, center_x] = channel_src[center_y, center_x]
+
+    # return the boundary vector as a 1D array
+    return b.flatten()
+
 
 
 def poisson_blend(im_src, im_tgt, im_mask, center):
+    """
+    Perform Poisson blending of the source image onto the target image
+    within the given mask region, centered at the given point.
+    """
+
+    # Get image dimensions
+    M, N, channels = im_tgt.shape
+
+    # Create sparse Laplacian operator matrix
+    laplacian = create_laplacian_matrix(M, N)
+
+    # Create sparse identity matrix
+    identity = eye(laplacian.shape[0], format='csr')
+
+    # Create sparse mask matrix
+    mask_indices = np.where(im_mask.flatten())
+    mask_matrix = lil_matrix((M*N, M*N))
+    # mask_matrix[mask_indices[0], mask_indices[0]] = 1
+
+    A = mask_matrix.dot(identity - laplacian)
+
+    # Compute blended image for each color channel
+    blended_channels = []
+    for channel in range(im_src.shape[2]):
+        # Create boundary vector b
+        b = create_boundary_vector(im_src[:, :, channel], im_tgt[:, :, channel], im_mask, center, laplacian)
+
+        # Solve the Poisson equation to obtain the blended image for this channel
+        x = spsolve(A, b)
+        x = np.clip(x, 0, 1)
+
+        blended_channels.append(x.reshape(im_src.shape[:2]))
+
+    # Merge blended channels into RGB image
+    blended = np.stack(blended_channels, axis=2)
+
+    return blended
+
+def poisson_blending(im_src, im_tgt, im_mask, center):
     """
     Performs Poisson blending of a source image onto a target image using a mask.
 
@@ -53,13 +147,13 @@ def poisson_blend(im_src, im_tgt, im_mask, center):
     mask = im_mask.flatten()
 
     # Compute Laplacian operator for target image
-    lap_target = laplacian_operator(M * N)
+    lap_target = compute_laplacian(im_tgt)
 
     # Construct sparse matrix A for Poisson equation
-    A = diags([mask], [0], shape=(M * N, M * N)) + lap_target
+    A = diags([mask], [0], format="lil") + lap_target
 
     # Compute Laplacian of source patch
-    lap_source = laplacian_operator(M * N) * src_patch.reshape(-1, 3)
+    lap_source = lap_target * src_patch.reshape(-1, 3)
 
     # Solve Poisson equation to obtain blended patch
     blended_patch_flat = spsolve(A, lap_source).reshape(-1, 3)
@@ -73,19 +167,19 @@ def poisson_blend(im_src, im_tgt, im_mask, center):
     im_blend = im_tgt
     return im_blend
 
-# # Load source, target, and mask images
-# im_src = cv2.imread('source.jpg')
-# im_tgt = cv2.imread('target.jpg')
-# im_mask = cv2.imread('mask.jpg', cv2.IMREAD_GRAYSCALE)
-#
-# # Specify center point for placing source image in target image
-# center = (100, 100)
-#
-# # Perform Poisson blending
-# im_blended = poisson_blend(im_src, im_tgt, im_mask, center)
-#
-# # Save blended image
-# cv2.imwrite('blended.jpg', im_blended)
+def poisson_blending_2(im_src, im_tgt, im_mask, center):
+    # Compute Laplacian operator for each color channel
+    laplacian_matrix = create_laplacian_matrix(im_mask)
+
+    # Solve Poisson equation for each color channel
+    im_blend = np.zeros_like(im_src)
+    for i in range(im_src.shape[2]):
+        A = laplacian_matrix
+        b = create_boundary_vector(im_src[:, :, i], im_tgt[:, :, i], im_mask, center)
+        x = sparse.linalg.spsolve(A, b)
+        im_blend[:, :, i] = x.reshape(im_src.shape[:2])
+
+    return im_blend
 
 def parse():
     parser = argparse.ArgumentParser()
@@ -107,7 +201,6 @@ if __name__ == "__main__":
         im_mask = cv2.threshold(im_mask, 0, 255, cv2.THRESH_BINARY)[1]
 
     center = (int(im_tgt.shape[1] / 2), int(im_tgt.shape[0] / 2))
-
     im_clone = poisson_blend(im_src, im_tgt, im_mask, center)
 
     cv2.imshow('Cloned image', im_clone)
---
+++ poisson_test.py
@@ -0,0 +1,120 @@
+import argparse
+import cv2
+from scipy.sparse.linalg import spsolve
+import numpy as np
+from scipy.sparse import diags
+
+
+def create_laplacian_matrix(width, height):
+    N = width * height
+
+    # Diagonal entries
+    d0 = np.ones(N) * -4
+    d1 = np.ones(N - 1)
+    d2 = np.ones(N - width)
+
+    # Create diagonals of the matrix
+    diags_list = [d0, d1, d1, d2, d2]
+    diags_pos = [0, -1, 1, -width, width]
+
+    # Create sparse matrix
+    laplacian = diags(diags_list, diags_pos, shape=(N, N))
+
+    # Set boundary conditions
+    laplacian = laplacian.tolil()
+    for i in range(width):
+        laplacian[i, i] = -3
+        laplacian[N - i - 1, N - i - 1] = -3
+    for i in range(height):
+        laplacian[i * width, i * width] = -3
+        laplacian[(i + 1) * width - 1, (i + 1) * width - 1] = -3
+
+    # Convert back to CSR format
+    laplacian = laplacian.tocsr()
+
+    return laplacian
+
+
+def create_boundary_vector(im_src, im_tgt, im_mask, center, laplacian):
+    # Create mask for boundary pixels
+    mask_boundary = np.logical_and(im_mask, 1 - cv2.erode(im_mask, None))
+
+    # Create boundary indices
+    boundary_indices = np.arange(im_src.size)[mask_boundary.flatten()]
+
+    # Create boundary vector
+    boundary_vector = np.zeros(im_src.size)
+    boundary_vector[boundary_indices] = im_tgt.flatten()[boundary_indices] - im_src.flatten()[boundary_indices]
+
+    # Subtract the laplacian of the source image from the boundary vector
+    boundary_vector[boundary_indices] -= laplacian.dot(im_src.flatten())[boundary_indices]
+
+    # Set the center pixel of the boundary vector to the corresponding pixel in the source image
+    boundary_vector[center[0]] = im_src.flatten()[center[0]]
+    boundary_vector[center[1]] = im_src.flatten()[center[1]]
+
+    return boundary_vector.flatten().reshape(-1, 1)
+
+
+def poisson_blend(im_src, im_tgt, im_mask, center):
+    """
+    Perform Poisson blending between a source and target image using a binary mask.
+
+    Args:
+        im_src (ndarray): The source image to be blended.
+        im_tgt (ndarray): The target image onto which the source will be blended.
+        im_mask (ndarray): A binary mask indicating the region of the source to be blended.
+        center (tuple): The (x,y) coordinates of the center of the source region.
+
+    Returns:
+        ndarray: The blended image.
+    """
+    # Split image into channels
+    im_src_channels = np.rollaxis(im_src, 2)
+    im_tgt_channels = np.rollaxis(im_tgt, 2)
+
+    # Initialize blended image
+    im_blend_channels = np.zeros_like(im_src_channels)
+
+    # Create Laplacian matrix
+    laplacian = create_laplacian_matrix(im_src.shape[:2][0], im_src.shape[:2][1])
+
+    # Iterate over channels
+    for i in range(im_src_channels.shape[0]):
+        # Create boundary vector
+        b = create_boundary_vector(im_src_channels[i], im_tgt_channels[i], im_mask, center, laplacian)
+
+        # Solve the system using sparse matrices
+        x = spsolve(laplacian, b)
+
+        # Reshape solution into image dimensions
+        im_blend_channels[i] = x.reshape(im_src.shape[:2])
+
+    # Merge channels and return blended image
+    return np.rollaxis(im_blend_channels, 0, 3)
+
+def parse():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--src_path', type=str, default='./data/imgs/banana2.jpg', help='image file path')
+    parser.add_argument('--mask_path', type=str, default='./data/seg_GT/banana1.bmp', help='mask file path')
+    parser.add_argument('--tgt_path', type=str, default='./data/bg/table.jpg', help='mask file path')
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    # Load the source and target images
+    args = parse()
+
+    im_tgt = cv2.imread(args.tgt_path, cv2.IMREAD_COLOR)
+    im_src = cv2.imread(args.src_path, cv2.IMREAD_COLOR)
+    if args.mask_path == '':
+        im_mask = np.full(im_src.shape, 255, dtype=np.uint8)
+    else:
+        im_mask = cv2.imread(args.mask_path, cv2.IMREAD_GRAYSCALE)
+        im_mask = cv2.threshold(im_mask, 0, 255, cv2.THRESH_BINARY)[1]
+
+    center = (int(im_tgt.shape[1] / 2), int(im_tgt.shape[0] / 2))
+    im_clone = poisson_blend(im_src, im_tgt, im_mask, center)
+
+    cv2.imshow('Cloned image', im_clone)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
