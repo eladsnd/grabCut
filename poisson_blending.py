7633bfef3772913657be442d034970ee271e3bbc
Elad <eladsnd@gmail.com>
2023-05-02 19:36:23 +0300

    problem masks

--- poisson_blending.py
+++ poisson_blending.py
@@ -1,9 +1,12 @@
+import os
+
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.sparse
 from scipy.sparse.linalg import spsolve
 import argparse
+from grabcut import grabcut
 
 
 def laplacian_matrix(n, m):
@@ -20,14 +23,28 @@ def laplacian_matrix(n, m):
     return mat_A
 
 
-def reshape_image(img_src, img_mask, center, x_odd, y_odd):
+def reshape_image(img_src, img_mask, center, img_tgt):
+    # x_odd = img_tgt.shape[0] % 2
+    # y_odd = img_tgt.shape[1] % 2
+    x_odd = 0
+    y_odd = 0
     padding_x = abs(int(center[1] - (img_src.shape[0] / 2)))
     padding_y = abs(int(center[0] - (img_src.shape[1] / 2)))
 
-    pad_src = np.pad(img_src, ((padding_x + x_odd, padding_x), (padding_y + y_odd, padding_y), (0, 0)),
+    if img_tgt.shape[0] > img_src.shape[0] + padding_x * 2:
+        x_odd = 1
+    if img_tgt.shape[1] > img_src.shape[1] + padding_y * 2:
+        y_odd = 1
+
+    if img_tgt.shape[0] < img_src.shape[0] + padding_x * 2:
+        x_odd -= 1
+    if img_tgt.shape[1] < img_src.shape[1] + padding_y * 2:
+        y_odd -= 1
+
+    pad_src = np.pad(img_src, ((padding_x, padding_x + x_odd), (padding_y, padding_y + y_odd), (0, 0)),
                      mode='constant', constant_values=0)
 
-    pad_mask = np.pad(img_mask, ((padding_x + x_odd, padding_x), (padding_y + y_odd, padding_y)),
+    pad_mask = np.pad(img_mask, ((padding_x, padding_x + x_odd), (padding_y, padding_y + y_odd)),
                       mode='constant', constant_values=0)
 
     return pad_src, pad_mask
@@ -44,10 +61,8 @@ def get_range(img_target):
 def poisson_blend(im_src, im_tgt, im_mask, center):
     # x_odd = 0 if img_tgt.shape[0] % 2 == 0 else 1
     # y_odd = 0 if img_tgt.shape[1] % 2` `== 0 else 1
-    x_odd = im_tgt.shape[0] % 2
-    y_odd = im_tgt.shape[1] % 2
 
-    pad_src, pad_mask = reshape_image(im_src, im_mask, center, x_odd, y_odd)
+    pad_src, pad_mask = reshape_image(im_src, im_mask, center, im_tgt)
 
     A = laplacian_matrix(pad_mask.shape[0], pad_mask.shape[1])
     lap = A.tocsc()
@@ -95,6 +110,39 @@ def poisson_blend(im_src, im_tgt, im_mask, center):
     return im_blend
 
 
+def run_on_grabcut_masks():
+    images = ['llama', 'memorial', 'sheep', 'stone2', 'fullmoon', 'teddy']
+    images = ['fullmoon', 'teddy', 'stone2']
+    images = ['sheep']
+
+    bgs = ['grass_mountains.jpeg', 'table.jpg', 'wall.jpg']
+    imgs_path = './data/imgs/'
+    masks_path = './data/masks/'
+    bgs_path = './data/bg/'
+    for img in images:
+        for bg in bgs:
+            for i in range(1):
+                try:
+                    im_tgt = cv2.imread(bgs_path + bg, cv2.IMREAD_COLOR)
+                    im_src = cv2.imread(imgs_path + img + '.jpg', cv2.IMREAD_COLOR)
+                    im_mask = cv2.imread(masks_path + img + '.bmp', cv2.IMREAD_GRAYSCALE)
+                    img_mask_seg_GT = cv2.imread('./data/seg_GT/' + img + '.bmp', cv2.IMREAD_GRAYSCALE)
+                    if im_tgt.shape[0] < im_src.shape[0] or im_tgt.shape[1] < im_src.shape[1]:
+                        continue
+                    if i == 0:
+                        im_blend = poisson_blend(im_src, im_tgt, im_mask, (im_tgt.shape[1] / 2, im_tgt.shape[0] / 2))
+                        if not os.path.exists('data/results/'):
+                            os.makedirs('data/results')
+                        cv2.imwrite('data/results/' + img + '_' + bg.split('.')[0] + '_our_mask.jpg', im_blend)
+                    if i == 1:
+                        im_blend = poisson_blend(im_src, im_tgt, img_mask_seg_GT,
+                                                 (im_tgt.shape[1] / 2, im_tgt.shape[0] / 2))
+                        cv2.imwrite('data/results/' + img + '_' + bg.split('.')[0] + '_seg_GT.jpg', im_blend)
+                    print('Saved ' + img + '_' + bg.split('.')[0] + '.jpg')
+                except(ValueError, TypeError, AttributeError):
+                    print('Failed ' + img + '_' + bg.split('.')[0] + '.jpg')
+
+
 def parse():
     parser = argparse.ArgumentParser()
     parser.add_argument('--src_path', type=str, default='./data/imgs/banana2.jpg', help='image file path')
@@ -106,7 +154,6 @@ def parse():
 if __name__ == "__main__":
     # Load the source and target images
     args = parse()
-
     im_tgt = cv2.imread(args.tgt_path, cv2.IMREAD_COLOR)
     im_src = cv2.imread(args.src_path, cv2.IMREAD_COLOR)
     if args.mask_path == '':
